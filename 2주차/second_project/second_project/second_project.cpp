b994973ae57a8ebad3de47f255d579c7770d8cb2
기상 <kisangyoo00@gmail.com>
2023-10-10 00:54:43 +0900

    2주 차 및 3주차

---
+++ 2주차/second_project/second_project/second_project.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <conio.h> // getch 함수를 사용하기 위해 추가
+#include <thread>
+
+using namespace std;
+
+int main() {
+    // 스플래시 이미지 출력
+    cout << "kisang's project" << endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    char command;
+    bool isRunning = true;
+
+    while (isRunning) {
+        // 커맨드 입력을 받고 출력
+        cout << "command> ";
+        command = _getch(); // 한 글자 입력 받음
+        cout << command << endl;
+
+        switch (command) {
+        case 'q':
+            cout << "qwer 화이팅" << endl;
+            break;
+        case 'w':
+            cout << "과제 너무 좋다" << endl;
+            break;
+        case 'e':
+            cout << "담주부턴 과제량 3배다" << endl;
+            break;
+        case 'r':
+            cout << "행복합니다." << endl;
+            break;
+        case 27: // ESC 키 누르면 종료
+            isRunning = false;
+            break;
+        default:
+            cout << "오류: 유효하지 않은 입력입니다." << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ 3주차/third_project/third_project/third_project.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    char inputBuffer[256]; // 입력을 저장할 버퍼
+    int bufferIndex = 0; // 현재까지 입력된 문자의 개수
+
+    cout << "키보드 입력을 3초 동안 받습니다. (3초 후에 종료됩니다)" << endl;
+
+    // 현재 시간 기록
+    auto startTime = chrono::high_resolution_clock::now();
+    auto currentTime = startTime;
+
+    // 3초 동안 입력 받기
+    while (chrono::duration_cast<chrono::seconds>(currentTime - startTime).count() < 3) {
+        if (_kbhit()) {
+            char ch = _getch(); // 키보드 입력 받기
+            inputBuffer[bufferIndex] = ch;
+            bufferIndex++;
+
+            // 엔터 키를 입력하면 입력 종료
+            if (ch == '\r') {
+                break;
+            }
+        }
+
+        // 현재 시간 업데이트
+        currentTime = chrono::high_resolution_clock::now();
+    }
+
+    // 입력 결과 출력
+    cout << "3초 동안 입력 받은 내용: ";
+    for (int i = 0; i < bufferIndex; i++) {
+        cout << inputBuffer[i];
+    }
+    cout << endl;
+
+    return 0;
+}
+;
